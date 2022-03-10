166eeb9b7e2ac1e9d03807ad7ce7644dc1e80271
Mike Ofori <61565391+monk63@users.noreply.github.com>
2022-03-10 11:47:56 +0000

    Create StatelessWidgetsAndHotReload.dart

---
+++ StatelessWidgetsAndHotReload.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+      ),
+    );
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Garage'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          'Splash Page',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.deepOrange[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Text('click'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    );
+  }
+}
